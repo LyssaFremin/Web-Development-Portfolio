a25ff13645efd86245617f4d7ae5aa388db6c9ef
Lyssa Fremin <lyssa.fremin@gmail.com>
2020-09-23 20:46:56 -0500

    Added navigation
    Added slide out naviation to right side of page
    
    Continued to add tweaks to redesign

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,8 +1,12 @@
-// $(function(){
-//   $("#jobHeader").hover(insertHeaderJob, removeHeader);
-// });
 
 $(document).ready(insertHeaderJob);
+
+$(function(){
+  $("#close-nav-btn").click(closeNav);
+  $("#nav-bar-button").click(openNav);
+});
+
+
 
 
 // Function to add random job title to header
@@ -14,9 +18,30 @@ function insertHeaderJob() {
   var index = 0;
 
   setInterval (function() {
-    $("#indirectArticle").html(article[index]);
-    $("#jobHeader").html(jobs[index]); // writes array to header
+    $("#indirect-article").html(article[index]);
+    $("#job-header").html(jobs[index]); // writes array to header
     index = (index + 1) % numJobs;
     console.log(index);   // increments index by 1 and uses modulus to set index for next element
   }, 1000);                            // Delay 1 second
 }
+
+function openNav() {
+  $("#side-nav").addClass("navWidth");
+  $("#main-nav").addClass("navMargin");
+}
+
+function closeNav() {
+  $("#side-nav").removeClass("navWidth");
+  $("#main-nav").removeClass("navMargin");
+}
+// /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+// function openNav() {
+//   document.getElementById("mySidenav").style.width = "250px";
+//   document.getElementById("main").style.marginLeft = "250px";
+// }
+//
+// /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+// function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+//   document.getElementById("main").style.marginLeft = "0";
+// }
